3e35dfc8b8876100c7ceed356cd23d0f61c0e722
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2025-07-17 16:19:03 +0200

    fix: missing brackets

--- routes/game.js
+++ routes/game.js
@@ -33,6 +33,7 @@ router.post('/api/setup-game', is_authenticated, async (req, res) => {
             execute_cfg_on_server(server_id, './cfg/nade_training.cfg');
         } else if (game_mode == "4") {
             execute_cfg_on_server(server_id, './cfg/deathmatch.cfg');
+        }
         // rcon.rcons[server_id].execute(`mp_warmup_pausetimer 1`);
         // rcon.rcons[server_id].execute(`changelevel ${selected_map}`);
         await rcon.execute_command(server_id, `mp_warmup_pausetimer 1`);
